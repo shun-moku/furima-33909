ce53991f4773b905146759581fbc47cfba298b0d
shun-moku <prmnmbr13.blue828@gmail.com>
2021-02-02 18:09:24 +0900

    aplication_controllerに記述するものだった

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,2 @@
 module ApplicationHelper
-  before_action :basic_auth
-
-
-  private
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-    end
-  end
 end
