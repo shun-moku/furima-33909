2e7259de85e5e275c40823b2a45c5f1ddded1869
shun-moku <prmnmbr13.blue828@gmail.com>
2021-02-04 17:58:59 +0900

    マイグレーション

--- db/migrate/20210204074134_devise_create_users.rb
+++ db/migrate/20210204074134_devise_create_users.rb
@@ -6,6 +6,13 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :nickname,           null: false
+      t.string :family_kanji,       null: false
+      t.string :first_kanji,        null: false
+      t.string :family_kana,        null: false
+      t.string :first_kana,         null: false
+      t.date   :birthday,           null: false
+
 
       ## Recoverable
       t.string   :reset_password_token
