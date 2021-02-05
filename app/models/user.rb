732c51c7c34b252cd321cab554946549119466c1
shun-moku <prmnmbr13.blue828@gmail.com>
2021-02-05 15:10:22 +0900

    バリデーション設定（英数含む、全角、全角かな）

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+
   validates :nickname,        presence: true
-  validates :family_kanji,    presence: true
-  validates :first_kanji,     presence: true
-  validates :family_kana,     presence: true
-  validates :first_kana,      presence: true
+  
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+    validates :family_kanji
+    validates :first_kanji
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください' } do
+    validates :family_kana,     presence: true
+    validates :first_kana,      presence: true
+  end
+
   validates :birthday,        presence: true
 end
