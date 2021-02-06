27087c6cdd258089adf56f9b7413b3177987b8f4
shun-moku <prmnmbr13.blue828@gmail.com>
2021-02-06 17:24:44 +0900

    日本語以外のfaker

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {'test'}
-    email                 {'test@example'}
-    password              {'000000a'}
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     family_kanji          {'山田'}
     first_kanji           {'太郎'}
     family_kana           {'ヤマダ'}
     first_kana            {'タロウ'}
-    birthday              {'2021-12-24'}
+    birthday              {Faker::Date.between(from: '1900-09-23', to: '2020-09-25')}
   end
 end
