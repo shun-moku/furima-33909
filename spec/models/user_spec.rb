f631a15a2f6160387b172658e56c0a884b6fe547
shun-moku <prmnmbr13.blue828@gmail.com>
2021-02-06 15:19:13 +0900

    単体テストコード_1

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
     it 'nicknameが空では登録できない' do
       # nicknameが空では登録できないテストコードを記述します
-      user = User.new(nickname: '', email: 'test@example', password: '000000', password_confirmation: '000000', family_kanji: '山田', first_kanji: '太郎', family_kana: 'ヤマダ', first_kana: 'タロウ')
+      user = User.new(nickname: '', email: 'test@example', password: '000000a', password_confirmation: '000000a', family_kanji: '山田', first_kanji: '太郎', family_kana: 'ヤマダ', first_kana: 'タロウ', birthday: '')
       user.valid?
       expect(user.errors.full_messages).to include("Nickname can't be blank")
     end
     it 'emailが空では登録できない' do
-      user = User.new(nickname: 'test', email: '', password: '000000', password_confirmation: '000000', family_kanji: '山田', first_kanji: '太郎', family_kana: 'ヤマダ', first_kana: 'タロウ')
+      user = User.new(nickname: 'test', email: '', password: '000000a', password_confirmation: '000000a', family_kanji: '山田', first_kanji: '太郎', family_kana: 'ヤマダ', first_kana: 'タロウ', birthday: '')
       user.valid?
       binding.pry
     end
