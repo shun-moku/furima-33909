5487b227080a468189b658c1522192045c8599f4
shun-moku <prmnmbr13.blue828@gmail.com>
2021-02-06 10:56:15 +0900

    単体テストコードその１

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+    it 'nicknameが空では登録できない' do
+      # nicknameが空では登録できないテストコードを記述します
+      user = User.new(nickname: '', email: 'test@example', password: '000000', password_confirmation: '000000', family_kanji: '山田', first_kanji: '太郎', family_kana: 'ヤマダ', first_kana: 'タロウ')
+      user.valid?
+      expect(user.errors.full_messages).to include("Nickname can't be blank")
+    end
+    it 'emailが空では登録できない' do
+      user = User.new(nickname: 'test', email: '', password: '000000', password_confirmation: '000000', family_kanji: '山田', first_kanji: '太郎', family_kana: 'ヤマダ', first_kana: 'タロウ')
+      user.valid?
+      binding.pry
+    end
+  end
 end
