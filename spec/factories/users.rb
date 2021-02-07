0cd29fa9b50a1e43db7920a5a030ce6270b7dcd3
shun-moku <prmnmbr13.blue828@gmail.com>
2021-02-07 15:12:54 +0900

    英数混合に編集

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password}
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation {password}
     family_kanji          {'山田'}
     first_kanji           {'太郎'}
