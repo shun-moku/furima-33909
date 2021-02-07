59940021f60da7b9f3b0d73d394bdfc6fc1b2366
shun-moku <prmnmbr13.blue828@gmail.com>
2021-02-07 16:54:59 +0900

    bundle exec rubocop -a

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
-  validates :nickname,        presence: true
-  
+  validates :nickname, presence: true
+
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
     validates :family_kanji
     validates :first_kanji
@@ -19,5 +19,5 @@ class User < ApplicationRecord
     validates :first_kana,      presence: true
   end
 
-  validates :birthday,        presence: true
+  validates :birthday, presence: true
 end
