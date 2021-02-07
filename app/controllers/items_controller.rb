f5cbccf162bff93f24d80eef92c84e8f2bb44512
shun-moku <prmnmbr13.blue828@gmail.com>
2021-02-07 22:10:35 +0900

    保存後ルートに遷移、ストロングパラメーターの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,18 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_charge_id, :region_id, :delivery_date_id, :private, :image).merge(user_id: current_user.id)
   end
 
 end
