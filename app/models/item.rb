160aab4a7d2dca438a29bec27316614c68def875
shun-moku <prmnmbr13.blue828@gmail.com>
2021-02-07 19:49:08 +0900

    マイグレーションとアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
+
+
+  validates :name,                presence: true
+  validates :description,         presence: true
+  validates :category_id,         presence: true
+  validates :condition_id,        presence: true
+  validates :shipping_charge_id,  presence: true
+  validates :region_id,           presence: true
+  validates :delivery_id,         presence: true
+  validates :price,               presence: true
+  validates :image,               presence: true
+
+  belongs_to: user
+
 end
