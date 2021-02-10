f516176ae700ddd4c1d41a1597bc4d7c9333d7d3
shun-moku <prmnmbr13.blue828@gmail.com>
2021-02-10 16:22:46 +0900

    move_to_index削除→editにまとめた

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit]
-  before_action :move_to_index, only: [:edit, :destroy]
+  before_action :set_item, only: [:edit, :show, :update]
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -19,15 +19,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
+    unless current_user.id == @item.user.id
+      redirect_to action: :index
+    end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -42,10 +42,8 @@ class ItemsController < ApplicationController
                                  :delivery_date_id, :price, :image).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    item = Item.find(params[:id])
-    unless current_user.id === item.user.id
-      redirect_to action: :index
-    end
+  def set_item
+    @item = Item.find(params[:id])
   end
+
 end
