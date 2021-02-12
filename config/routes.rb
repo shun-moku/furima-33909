a8d6317ea1bb210ca4e4fa064b64a47b9ba2282b
shun-moku <prmnmbr13.blue828@gmail.com>
2021-02-12 17:37:36 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items do
-    resources :orders, only: :move_to_index
-  end
-  
-  
+  resources :items
 end
