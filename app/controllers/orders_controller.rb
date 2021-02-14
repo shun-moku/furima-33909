a4f12e3f9d0055732cf32c32e2b1e30c14c94219
shun-moku <prmnmbr13.blue828@gmail.com>
2021-02-14 11:21:33 +0900

    商品購入機能追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,36 @@
 class OrdersController < ApplicationController
  
   def index
+    @order_address = OrderAddress.new
+    @item = Item.find(params[:item_id])
   end
 
+  def create
+    @order_address = OrderAddress.new(order_params)
+    @item = Item.find(params[:item_id])
+    if @order_address.valid?
+      pay_item
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    # params.permit(:price).merge(user_id: current_user.id)
+    params.require(:order_address).permit(:zip_code, :region_id, :city, :street_number, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
+  end
 
 end
---
+++ app/models/order_address.rb
@@ -0,0 +1,24 @@
+class OrderAddress
+  Address
+  include ActiveModel::Model
+  attr_accessor :zip_code, :city, :street_number, :building, :phone_number, :user_id, :item_id, :token, :region_id, :order_id
+
+  with_options presence: true do
+    validates :user_id
+    validates :zip_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city
+    validates :street_number
+    validates :phone_number
+    validates :token
+  end
+  validates :region_id, numericality: {other_than: 0, message: "can't be blank"}
+
+  def save
+    order = Order.create(item_id: item_id, user_id: user_id)
+
+    Address.create(zip_code: zip_code, region_id: region_id, city: city, street_number: street_number, building: building, order_id: order.id)
+  end
+
+
+
+end
