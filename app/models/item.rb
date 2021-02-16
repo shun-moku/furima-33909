11c0915f51aaedb240be7408d9e4abf0a13069e2
shun-moku <prmnmbr13.blue828@gmail.com>
2021-02-16 18:15:42 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,7 +19,7 @@ class Item < ApplicationRecord
     validates :image
   end
 
-  validates :price, numericality: { only_integer: true, greater_than: 300, less_than: 9_999_999 }
+  validates :price, numericality: { only_integer: true, greater_than: 299, less_than: 10000000 }
 
   belongs_to :user
   has_one_attached :image
